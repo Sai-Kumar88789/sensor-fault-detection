7e9ddaf22153123b28526b76c70d542c64322623
sai kumar <sai680513@gmail.com>
2022-11-17 10:12:32 +0530

    update main.py

--- get_data.py
+++ get_data.py
@@ -1,12 +1,12 @@
 file_path="/config/workspace/aps_failure_training_set1.csv"
 from sensor.data_access.sensor_data import SensorData
 from sensor.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
-from main import set_env_variable
+from dotenv import load_dotenv
+load_dotenv()
 import os
 if __name__=='__main__':
     data_file_path="/config/workspace/aps_failure_training_set1.csv"
     env_file_path='/config/workspace/env.yaml'
-    set_env_variable(env_file_path)
     print( os.environ['MONGO_DB_URL'])
     sd = SensorData()
     if DATA_INGESTION_COLLECTION_NAME in sd.mongo_client.database.list_collection_names():
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from sensor.configuration.mongodb_connection import MongoDBClient
-from sensor.exception import SensorException
+from sensor.configuration.mongodb_connection import MongodbClient
+from sensor.exception import Sensor_Exception
 import os,sys
 from sensor.logger import logging
 from sensor.pipelines import training_pipeline
@@ -18,15 +18,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import pandas as pd
 
-
 env_file_path=os.path.join(os.getcwd(),"env.yaml")
-
-def set_env_variable(env_file_path):
-
-    if os.getenv('MONGO_DB_URL',None) is None:
-        env_config = read_yaml_file(env_file_path)
-        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
-
 
 app = FastAPI()
 origins = ["*"]
